1402b6f1c91222a8703657853f6204078c8ce862
fanjunyang <junyang.fan378@imaygou.com>
2023-09-27 11:46:08 +0800

    build: 📦️ 修改构建流程

--- packages/.lintstagedrc.cjs
+++ packages/.lintstagedrc.cjs
@@ -1,3 +1,4 @@
 module.exports = {
-  '*.{js,ts,vue,jsx,tsx}': () => ['npm run lint', 'npm run lint:fix'],
+  // '*.{js,ts,vue,jsx,tsx}': () => ['npm run lint', 'npm run lint:fix'],
+  '*.{js,ts,vue,jsx,tsx}': () => ['yarn lint'],
 }
